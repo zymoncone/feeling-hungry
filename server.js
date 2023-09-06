ba7862fb459b45695088e082fc8bc2b4455ced48
Szymon Sarnowicz <szymon.sarnowicz@gm.com>
2023-09-06 01:46:30 -0500

    My first React App launch

---
+++ server.js
@@ -0,0 +1,35 @@
+const PORT = 8000
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+const OPENAI_API_KEY = process.env.API_KEY
+
+app.post('/completitions', async (req, res) => {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${OPENAI_API_KEY}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "user", content: "how are you?"}],
+            max_tokens: 100
+        })
+    }
+    try {
+        const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+        const data = await response.json()
+        res.send(data)
+
+    } catch (error) {
+        console.error(error)
+    }
+})
+
+app.listen(PORT, () => console.log('Your server is running on Port ' + PORT))
--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+
+  const getMessage = async () => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: "what is 5+5?" // this doesn't matter, gets sent to backend to pass to OPENAI
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+    try {
+      const response = await fetch('http://localhost:8000/completitions', options)
+      const data = await response.json()
+      console.log(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <section className="main">
+        <h1>Feeling Hungry?</h1>
+        <h4>Just enter below what you have and we'll tell you what you can make!</h4>
+        <button>clear chat box</button>
+        <p id="output"></p>
+        <div className="bottom-section">
+          <h3>What's in your fridge?</h3>
+            <div className="input-container">
+
+              <textarea id="entry" type="text" placeholder="(input as comma separated words)"></textarea>
+              
+              <div id="submit" onClick={getMessage}>
+                  ➱
+              </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
