e0e3d62e5f5aa6df4000dfede8f8b50cc534954e
Szymon Sarnowicz <szymon.sarnowicz@gm.com>
2023-09-07 11:36:18 -0500

    implement OpenAI response to display

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import {useState, useEffect} from 'react'
+
 function App() {
+
+  const [value, setValue] = useState("")
+  const [message, setMessage] = useState({content:''})
+
+  const clearInput = () => {
+    setValue("")
+    setMessage({content:''})
+  }
 
   const getMessage = async () => {
     const options = {
       method: "POST",
       body: JSON.stringify({
-        message: "what is 5+5?" // this doesn't matter, gets sent to backend to pass to OPENAI
+        message: value // this is what we are passing to backend. value comes from text area input
       }),
       headers: {
         "Content-Type": "application/json"
@@ -13,25 +23,27 @@ function App() {
     try {
       const response = await fetch('http://localhost:8000/completitions', options)
       const data = await response.json()
-      console.log(data)
+      setMessage(data.choices[0].message)
+
     } catch (error) {
       console.error(error)
     }
   }
 
+  console.log(message)
 
   return (
     <div className="app">
       <section className="main">
         <h1>Feeling Hungry?</h1>
         <h4>Just enter below what you have and we'll tell you what you can make!</h4>
-        <button>clear chat box</button>
-        <p id="output"></p>
+        <button onClick={clearInput}>clear chat box</button>
+        {message.content != "" && <p id="output">{message.content}</p>}
         <div className="bottom-section">
           <h3>What's in your fridge?</h3>
             <div className="input-container">
 
-              <textarea id="entry" type="text" placeholder="(input as comma separated words)"></textarea>
+              <textarea id="entry" type="text" placeholder="(input as comma separated words)" value={value} onChange={(e) => setValue(e.target.value)}></textarea>
               
               <div id="submit" onClick={getMessage}>
                   ➱
